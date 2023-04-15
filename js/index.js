8b9670d62494ddc979a8224d86856807d6e4ea82
manas.sohgaura <60660619+manas-codes@users.noreply.github.com>
2023-04-16 02:32:23 +0530

    fix: scroll issues

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ const mainLink = document.querySelector("#main-link");
       mainLink.addEventListener("click", function(event) {
         event.preventDefault();
         const fragmentIdentifier = mainLink.getAttribute("href");
-        const targetSection = document.querySelector(fragmentIdentifier);
+        const targetSection = document.querySelector("#content");
         if (targetSection) {
           targetSection.scrollIntoView({ behavior: "smooth" });
         }
